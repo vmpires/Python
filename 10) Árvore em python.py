e8040bc09a6064c0ec58931d354892549626a38f
Victor Milhome Pires <77033013+vmpires@users.noreply.github.com>
2021-01-15 00:35:36 -0300

    Add files via upload

---
+++ 10) Árvore em python.py
@@ -0,0 +1,19 @@
+rows = input ('Qual o tamanho da sua árvore de natal digital? Digite um número. ')
+
+rows = int(rows)
+
+x = '*'
+y = ''
+z = 48
+for i in range(0,rows*2,2):
+ i = i+1
+ print (y.center(z),x * i)
+ z = z-1
+ 
+
+tronco = x.center (100)
+print (tronco)
+print (tronco)
+print (tronco)
+
+input()
