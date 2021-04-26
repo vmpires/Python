9dd04fb4d5ebddcc694951a13b8eff9cc441bfb4
Victor Pires <vctprs@gmail.com>
2021-04-26 12:19:16 -0300

    Melhoria 36

--- 36)Simulador de Banco Simples - Saldo após operações.py
+++ 36)Simulador de Banco Simples - Saldo após operações.py
@@ -1,14 +1,14 @@
 listacontas = [["12345", 50], ["12345", -20], ["98765", 25], ["98765", 50]]
-dictids = {}
+saldos = {}
 
-for item in listacontas:
-    dictids[item[0]] = 0
+for conta in listacontas:
+    saldos[conta[0]] = 0
 
-for item in listacontas:
-    if item[0] in dictids:
-        dictids[item[0]] += item[1]
+for conta in listacontas:
+    if conta[0] in saldos:
+        saldos[conta[0]] += conta[1]
 
-for item, item2 in dictids.items():
-    print(f'Conta: {item} / Saldo: {item2}')
+for conta, saldo in saldos.items():
+    print(f'Conta: {conta} / Saldo: {saldo}')
 
 
